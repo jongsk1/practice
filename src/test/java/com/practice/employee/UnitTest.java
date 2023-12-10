3272fac01f9802c6126897cd1e1f6545c7d5c2ee
김종석 <qwoop87@gmail.com>
2023-12-10 18:25:29 +0900

    fix : UnitTest 클래스에 objectmapper 추가 및 config 설정 변경

--- src/test/java/com/practice/employee/UnitTest.java
+++ src/test/java/com/practice/employee/UnitTest.java
@@ -1,5 +1,8 @@
 package com.practice.employee;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -8,4 +11,11 @@ import org.springframework.test.web.servlet.MockMvc;
 public abstract class UnitTest {
   protected MockMvc mockMvc;
   protected final String BASE_PATH = "/api/employee";
+  protected ObjectMapper objectMapper;
+
+  @BeforeEach
+  void init() {
+    this.objectMapper = new ObjectMapper();
+    this.objectMapper.registerModule(new JavaTimeModule());
+  }
 }
